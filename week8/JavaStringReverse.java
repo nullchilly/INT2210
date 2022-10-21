6d8852d51b32832fdfb932b442df46c50d4ff8da
Null Chilly <nullchilly@gmail.com>
2022-10-21 15:23:43 +0700

    chore: week 8

--- week8/JavaStringReverse.java
+++ week8/JavaStringReverse.java
@@ -1,2 +1,9 @@
-package week8;public class JavaStringReverse {
+import java.util.Scanner;
+
+public class JavaStringReverse {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.nextLine();
+    System.out.println(str.equals(new StringBuilder(str).reverse().toString()) ? "Yes" : "No");
+  }
 }
